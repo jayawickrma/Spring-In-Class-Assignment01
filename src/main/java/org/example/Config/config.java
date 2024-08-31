7d32e1eaeaa822bb378fee0ff5d352c064d6fdb2
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-08-31 10:49:59 +0530

    sfssa

--- src/main/java/org/example/Config/config.java
+++ src/main/java/org/example/Config/config.java
@@ -1,9 +1,15 @@
 package org.example.Config;
 
+import org.example.beans.ChickenRoll;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = {"org.example"})
 public class config {
+    @Bean
+    ChickenRoll chickenRoll(){
+        return chickenRoll();
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,14 @@
 package org.example;
 
+import org.example.Config.config;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext();
-        annotationConfigApplicationContext.register();
+        annotationConfigApplicationContext.register(config.class);
         annotationConfigApplicationContext.refresh();
+
 
 
 
